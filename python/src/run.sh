e8a3680254b68e47ce0a2c861bb8cebe7b43c7a2
danielmoreirapt <danielmoreirapt@gmail.com>
2019-10-13 17:39:32 +0100

    Final Dockerfile

--- python/src/run.sh
+++ python/src/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-python3 utils.py
-python3 drive_through.py
-python3 cook.py
-python3 deliver.py
-python3 clerk_example.py
-python3 client_example.py
+python3 src/drive_through.py &
+python3 src/cook.py &
+python3 src/deliver.py &
+python3 src/clerk_example.py &
+python3 src/client_example.py &
